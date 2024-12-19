121c28d7a2addf328e31d010fe742dff63375112
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2024-12-19 23:51:51 +0530

    feat: FE: logo menu search bar

--- front-end/src/app/header/logo-menu/logo-menu.component.ts
+++ front-end/src/app/header/logo-menu/logo-menu.component.ts
@@ -1,12 +1,13 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logo-menu',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './logo-menu.component.html',
   styleUrl: './logo-menu.component.scss'
 })
 export class LogoMenuComponent {
-
+  highlightSearchBar = false;
 }
